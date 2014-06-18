bf67371d355670dddf7d4c27e0a7c6094b0468ab
laserbot <rockymoran@gmail.com>
2014-06-18 16:45:18 -0700

    messing with db

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8080;
 
 // configuration ===============================================================
 
-mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'); 	// connect to mongoDB database on modulus.io
+mongoose.connect('localhost:27017'); 	// connect to mongoDB database
 
 app.configure(function() {
 	app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
