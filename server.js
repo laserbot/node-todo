98ed22251057fc9c67e20a07ecfe4a20f844af51
Rocky Moran <rockymoran@gmail.com>
2014-06-19 05:34:32 +0000

    testing server.js edits on nitrous

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8080;
 
 // configuration ===============================================================
 
-mongoose.connect('localhost:27017'); 	// connect to mongoDB database
+mongoose.connect('localhost:27017/todo'); 	// connect to mongoDB database
 
 app.configure(function() {
 	app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
